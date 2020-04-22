17c4771786de963810bab99d58e3e13ad6f993cb
HangqianLi <lihangqian_hit@163.com>
2020-04-22 09:49:52 +0200

    Initial Commit

---
+++ Render/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Render
+//
+//  Created by Neil Li on 2020/4/22.
+//  Copyright © 2020 Neil Li. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
